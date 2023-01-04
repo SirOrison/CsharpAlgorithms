9f01ae02e815d35da0bbe8e5dbf2661643f40053
SirOrison <--replace-all>
2023-01-04 16:34:29 +0000

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Emit;
 
 namespace TestConsole
 {
@@ -6,14 +7,43 @@ namespace TestConsole
     {
         static void Main(string[] arg)
         {
+            Car myCar = new Car("Oldsmobile", "Cutlas Supreme", 1986, "Silver");
 
-            Basics basics = new Basics();
+            Car myNewCar = new Car();
 
+
+            Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color.ToUpper());
+          
+
+            Console.ReadLine();
             
 
-            basics.MathFunction(-10,10);
         }
     }
-}
 
+    class Car
+    {
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public Car(string make, string model, int year, string color) 
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.Color = color;
+        }
+
+        public Car()
+        {
+            this.Make = "Nissan";
+        }
+
+     
+
+    }
+}
 
