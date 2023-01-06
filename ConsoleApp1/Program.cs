501922ee5873cc480c0714c96e9248a87c57a1d9
SirOrison <--replace-all>
2023-01-06 12:52:38 +0000

    updated with more classes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,65 +9,64 @@ namespace TestConsole
     {
         static void Main(string[] arg)
         {
-           
-           
-            Console.ReadLine();
-        }     
+            
+        }
+       
+
+        
     }
-
-    class Car
-    {
-        public string Make { get; set; }
-        public string  Model { get; set; }
-        public string VIN { get; set; }
-        public int Year { get; set; }
-        public double StickerPrice { get; set; }
-    }
-
-    class Book
-    {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string ISBN { get; set; }
-    }
-
-    /* List<Car> myCar = new List<Car>()
-            {
-                new Car {VIN="A1", Make="BMW", Model="550i", StickerPrice=55000, Year= 2009},
-                new Car {VIN="B2", Make="Toyota", Model="4Runner", StickerPrice=35000, Year= 2010},
-                new Car {VIN="C3", Make="BMW", Model="745li", StickerPrice=75000, Year= 2008},
-                new Car {VIN="D4", Make="Ford", Model="Escape", StickerPrice=25000, Year= 2008},
-                new Car {VIN="E5", Make="BMW", Model="55i", StickerPrice=57000, Year= 2010}
-            };*/
-
-    //LINQ query
-    /*var bmws = from car in myCar
-               where car.Make == "BMW"
-               && car.Year == 2010
-               select car;*/
-
-    /*var orderedCars = from car in myCar
-                     orderby car.Year descending
-                     select car;*/
-
-
-    //LINQ method
-
-    //var bmws = myCar.Where( car => car.Make == "BMW" && car.Year == 2010);
-
-    //var orderedCars = myCar.OrderByDescending(car => car.Year);
-
-
-   /* var FirstBmw = myCar.OrderByDescending(car => car.Year).First(car => car.Make == "BMW");
-
-
-    Console.WriteLine(FirstBmw.VIN);
-*/
-            /*foreach (var bm in orderedCars) {
-
-                Console.WriteLine("{0} {1} {2}", bm.Year, bm.Model, bm.VIN);
-            }*/
-
-    
+  
 }
 
+/*class Car
+   {
+       public string Make { get; set; }
+       public string  Model { get; set; }
+       public string VIN { get; set; }
+       public int Year { get; set; }
+       public double StickerPrice { get; set; }
+   }
+
+   class Book
+   {
+       public string Title { get; set; }
+       public string Author { get; set; }
+       public string ISBN { get; set; }
+   }
+*/
+/* List<Car> myCar = new List<Car>()
+        {
+            new Car {VIN="A1", Make="BMW", Model="550i", StickerPrice=55000, Year= 2009},
+            new Car {VIN="B2", Make="Toyota", Model="4Runner", StickerPrice=35000, Year= 2010},
+            new Car {VIN="C3", Make="BMW", Model="745li", StickerPrice=75000, Year= 2008},
+            new Car {VIN="D4", Make="Ford", Model="Escape", StickerPrice=25000, Year= 2008},
+            new Car {VIN="E5", Make="BMW", Model="55i", StickerPrice=57000, Year= 2010}
+        };*/
+
+//LINQ query
+/*var bmws = from car in myCar
+           where car.Make == "BMW"
+           && car.Year == 2010
+           select car;*/
+
+/*var orderedCars = from car in myCar
+                 orderby car.Year descending
+                 select car;*/
+
+
+//LINQ method
+
+//var bmws = myCar.Where( car => car.Make == "BMW" && car.Year == 2010);
+
+//var orderedCars = myCar.OrderByDescending(car => car.Year);
+
+
+/* var FirstBmw = myCar.OrderByDescending(car => car.Year).First(car => car.Make == "BMW");
+
+
+ Console.WriteLine(FirstBmw.VIN);
+*/
+/*foreach (var bm in orderedCars) {
+
+    Console.WriteLine("{0} {1} {2}", bm.Year, bm.Model, bm.VIN);
+}*/
---
+++ ConsoleApp1/TryAndCatch.cs
@@ -0,0 +1,39 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TryAndCatch
+    {
+        try
+        {
+            string content = File.ReadAllText(@"C:\testfiles\WriteTxt.txt");
+            Console.WriteLine(content);
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine("There was a problem");
+            Console.WriteLine(ex.Message);
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            Console.WriteLine("There was a problem");
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("There was a problem");
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine();
+
+        }
+        Console.ReadLine();
+    }
+}
+*/
